150b10a899723848177b7a2826f0668a66f2c1a9
BlaXXuN <p4dd1@gmx.de>
2015-11-13 13:58:26 +0100

    implements config file to load modules and set number of events

--- drift_chamber.py
+++ drift_chamber.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python3.4
 
 import logging
+from configparser import ConfigParser
 
 from core.run_engine import RunEngine
-from modules.HelloWorldModule import HelloWorld
-from modules.ByeByeWorldModule import ByeByeWorld
 
 
 
@@ -12,14 +11,39 @@ def main():
     logging.basicConfig(level=logging.DEBUG)
     logging.info('Drift Chamber project started')
 
-    helloWorldModule = HelloWorld()
-    byebyeWorldModule = ByeByeWorld()
+    config = ConfigParser()
+    config.read('config.cfg')
+    # Check if section Modules and option load_modules exists in config file and read the values
+    if config.has_option('Modules', 'load_modules'):
+        modules = config['Modules']['load_modules'].strip().split('\n')
+    else:
+        print('No Modules specified')
+        return
+
+    # Check if section General and option event_count exists in config file and read values
+    if config.has_option('General', 'event_count'):
+        event_count = config.getint('General', 'event_count')
+    else:
+        print('Event Count was not specified')
+        return
+
+    # Import modules specified in config file. Create objects of Classes in the module (note:
+    # the module Class has to have the same name as the file without Module at the end. e.g.
+    # filename: EventModule, classname: Event)
+    # Append created objects to module_list
+    module_list = []
+    for mod in modules:
+        tmp = __import__('modules.'+mod, globals(), locals(), [mod[:-6]])
+        module_list.append(getattr(tmp, mod[:-6])())
 
     runEngine = RunEngine()
-    runEngine.add_module(helloWorldModule)
-    runEngine.add_module(byebyeWorldModule)
-    runEngine.set_events(100)
 
+    # Register every module in module_list in runEngine
+    for mod in module_list:
+        runEngine.add_module(mod)
+    runEngine.set_events(event_count)
+
+    # Run simulation
     runEngine.run()
 
     logging.info('All processing done')
