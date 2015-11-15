e5c07c75cf6b8f09fd9df3b1d8e4ad61a1dd5db6
BlaXXuN <p4dd1@gmx.de>
2015-11-15 13:08:01 +0100

    implements usage of parameters to load config file, modules and set event_count
    usage: ``drift_chamber.py --config configfile``
    or ``drift_chamber.py --module module1 --module module2 --eventCount 100``
    
    this closes #2
    enables #1

--- driftchamber/drift_chamber.py
+++ driftchamber/drift_chamber.py
@@ -2,30 +2,54 @@
 
 import logging
 from configparser import ConfigParser
+from argparse import ArgumentParser
+import os
 
 from driftchamber.core.run_engine import RunEngine
 
 
 
 def main():
+    parser = ArgumentParser('Driftchamber Project of Team A')
+    parser.add_argument('--config', type=str, default=None,
+                        help='The configuration file to load. This overrides --modules and --eventCount.')
+    parser.add_argument('--module', type=str, default=None, action='append',
+                        help='Specify a module to load. Can be used multiple times to load multiple modules.')
+    parser.add_argument('--eventCount', type=int, default=None, help='Specify the amount of events to run.')
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.DEBUG)
     logging.info('Drift Chamber project started')
 
-    config = ConfigParser()
-    config.read('config.cfg')
-    # Check if section Modules and option load_modules exists in config file and read the values
-    if config.has_option('Modules', 'load_modules'):
-        modules = config['Modules']['load_modules'].strip().split('\n')
+    if args.config:
+        if not os.path.isfile(args.config):
+            logging.error("Specified config file does not exist")
+            return
+        config = ConfigParser()
+        config.read(args.config)
+        # Check if section Modules and option load_modules exists in config file and read the values
+        if config.has_option('Modules', 'load_modules'):
+            modules = config['Modules']['load_modules'].strip().split('\n')
+        else:
+            logging.error('No Modules specified')
+            return
+
+        # Check if section General and option event_count exists in config file and read values
+        if config.has_option('General', 'event_count'):
+            event_count = config.getint('General', 'event_count')
+        else:
+            logging.error('Event Count was not specified')
+            return
+    elif args.module and args.eventCount:
+        modules = args.module
+        event_count = args.eventCount
     else:
-        print('No Modules specified')
+        if not args.module:
+            logging.error('No modules to load.')
+        if not args.eventCount:
+            logging.error('Number of events (eventCount) has to be specified.')
         return
 
-    # Check if section General and option event_count exists in config file and read values
-    if config.has_option('General', 'event_count'):
-        event_count = config.getint('General', 'event_count')
-    else:
-        print('Event Count was not specified')
-        return
 
     # Import modules specified in config file. Create objects of Classes in the module (note:
     # the module Class has to have the same name as the file without Module at the end. e.g.
@@ -33,8 +57,15 @@ def main():
     # Append created objects to module_list
     module_list = []
     for mod in modules:
-        tmp = __import__('modules.'+mod, globals(), locals(), [mod[:-6]])
-        module_list.append(getattr(tmp, mod[:-6])())
+        try:
+            tmp = __import__('modules.'+mod, globals(), locals(), [mod[:-6]])
+        except ImportError:
+            logging.error('Specified Module does not exist or could not be loaded.')
+            return
+        try:
+            module_list.append(getattr(tmp, mod[:-6])())
+        except AttributeError as e:
+            logging.error('\''+mod[:-6]+'\' class was not found in module \''+mod+'\'')
 
     runEngine = RunEngine()
 
