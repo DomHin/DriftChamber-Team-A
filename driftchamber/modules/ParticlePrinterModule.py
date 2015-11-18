d4d7f86694c1ba3012a5a14dc3f0a5372556e873
Anna Friedel <uaijz@student.kit.edu>
2015-11-18 15:55:12 +0100

    Made the momentums a bit more physical Now the new modules run like HelloWorld and ByeByeWorld-Module, when called

--- driftchamber/modules/ParticlePrinterModule.py
+++ driftchamber/modules/ParticlePrinterModule.py
@@ -1,14 +1,25 @@
 __author__ = 'Patrick Schreiber'
 
 from driftchamber.core.module import Module
+import logging
 
 class ParticlePrinter(Module):
-
     def __init__(self):
         self.particle_name = 'SuperParticle'
 
     def begin(self, datastore):
-        print('Particle: {}'.format(self.particle_name))
+        # print("Begin of Simulation of ParticleGun")
+        logging.info('Begin of Simulation of ParticleGun')
+
+    def event(self, datastore):
         particle = datastore.get(self.particle_name)
-        print(particle.position())
-        print(particle.momentum())
+        logging.info('Particle: {}'.format(self.particle_name))
+
+        logging.info(particle.position())
+        logging.info(particle.momentum())
+
+    def end(self, datastore):
+        # print("End of Simulation of ParticleGun")
+        logging.info('End of Simulation of ParticleGun')
+
+
