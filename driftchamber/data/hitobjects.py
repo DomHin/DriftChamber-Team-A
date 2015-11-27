619d6adc4b6e3bbfc6870f5b5d0b8e7c2e237834
Jonas Wagner <jonas-f.wagner@web.de>
2015-11-27 23:32:29 +0100

    Hits should not be sorted by particles since they are only used for reconstruction and the modules using the hits therefore don't know about the particles.

--- driftchamber/data/hitobjects.py
+++ driftchamber/data/hitobjects.py
@@ -15,3 +15,17 @@ class HitObjects:
 
     def get_hits(self, event_number, particle_name):
         return self._objects[particle_name][event_number]
+
+    def get_all_hits(self, event_number):
+        """
+        This is used for reconstruction purposes. (Do we need to sort by particle name at all?)
+        :param event_number:
+        :return:
+        """
+        hits = []
+        for key in self._objects:
+            try:
+                hits += self._objects[key][event_number]
+            except KeyError:
+                pass
+        return hits
