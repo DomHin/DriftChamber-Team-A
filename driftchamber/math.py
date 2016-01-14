c4564315214129b350cd93ce287485167b1a9521
Genti Saliu <gentisaliu@gmail.com>
2016-01-14 17:06:55 +0100

    Complete SonarQube integration [ci skip]

--- driftchamber/math.py
+++ driftchamber/math.py
@@ -1,3 +1,52 @@
+from numpy import array
+
+
+class Point2D(array):
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    @property
+    def x(self):
+        return self[0]
+
+    @x.setter
+    def x(self, value):
+        self[0] = value
+
+    @property
+    def y(self):
+        return self[1]
+
+    @y.setter
+    def y(self, value):
+        self[1] = value
+
+
+class Dimension2D(array):
+
+    def __init(self, width, height):
+        super().__init__(width, height)
+
+    @property
+    def width(self):
+        return self[0]
+
+    @property
+    def height(self):
+        return self[1]
+
+
+class Rectangle2D(array):
+    
+    def __init__(self, pos, dim):
+        super().__init__([
+            [pos.x, pos.y],
+            [pos.x + dim.width, pos.y],
+            [pos.x, pos.y + dim.height],
+            [pos.x + dim.width, pos.y + dim.height]
+        ])
+
 def point_in_rect(point, rect):
     return point[0] >= rect[0][0] and \
             point[0] <= rect[1][0] and \
--- driftchamber/tests/test_math.py
+++ driftchamber/tests/test_math.py
@@ -1,23 +1,23 @@
 from unittest.case import TestCase
 from nose_parameterized import parameterized
-from numpy import array
-from driftchamber.math import point_in_rect, sign
+from driftchamber.math import Point2D, Rectangle2D, Dimension2D, \
+                                point_in_rect, sign
 
 
 class MathTest(TestCase):
 
     @parameterized.expand([
-        (array([4, 6]), array([[2, 3], [10, 3], [2, 13], [10, 13]])),
-        (array([10, 13]), array([[2, 3], [10, 3], [2, 13], [10, 13]])),
-        (array([4, 6]), array([[4, 6], [12, 6], [4, 16], [12, 16]]))
+        (Point2D(4, 6), Rectangle2D(Point2D(2, 3), Dimension2D(8, 10))),
+        (Point2D(10, 13), Rectangle2D(Point2D(2, 3), Dimension2D(8, 10))),
+        (Point2D(4, 6), Rectangle2D(Point2D(4, 6), Dimension2D(8, 10)))
     ])
     def test_point_in_rect_true(self, point, rect):
         self.assertTrue(point_in_rect(point, rect))
 
     @parameterized.expand([
-        (array([4, 13.01]), array([[2, 3], [10, 3], [2, 13], [10, 13]])),
-        (array([3, 6]), array([[4, 6], [12, 6], [4, 16], [12, 16]])),
-        (array([3, 17]), array([[4, 6], [12, 6], [4, 16], [12, 16]]))
+        (Point2D(4, 13.01), Rectangle2D(Point2D(2, 3), Dimension2D(8, 10))),
+        (Point2D(3, 6), Rectangle2D(Point2D(2, 3), Dimension2D(8, 10))),
+        (Point2D(3, 17), Rectangle2D(Point2D(4, 6), Dimension2D(8, 10)))
     ])
     def test_point_in_rect_false(self, point, rect):
         self.assertFalse(point_in_rect(point, rect))
